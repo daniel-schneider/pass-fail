73c5fbdd154e4e4fafce198c2709e2ab60130d8a
Dan Schneider <daniel.schneider@pop.belmont.edu>
2015-11-11 10:35:35 -0600

    passfail.java

---
+++ src/com/passfail/PassFail.java
@@ -0,0 +1,22 @@
+package com.passfail;
+import java.util.Scanner;
+
+/**
+ * Created by Dan on 9/24/2015.
+ */
+public class PassFail {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        System.out.println("What grade did you get on your test");
+        a = in.nextInt();
+        if (a < 50) {
+            System.out.println("too bad you failed");
+        }
+        else{
+                System.out.println("congrats! you passed!");
+            }
+        }
+    }
+
+
